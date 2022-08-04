8bcecdb39296e7af00476a6c8665ff27e0a920a6
Md Mehedi Ahsan <ahsanmehedi041@gmail.com>
2022-08-04 11:33:46 +0600

    function and parameter added

---
+++ function-parameter.js
@@ -0,0 +1,5 @@
+function fine(tk){
+    console.log('Your fine is : '+tk,'tk');
+}
+fine(50);
+
---
+++ function.js
@@ -0,0 +1,13 @@
+//function declaration
+
+function ourTeam(){
+    console.log('Mehedi');
+    console.log('Sohan');
+    console.log('Rakib');
+}
+
+//call function
+
+ourTeam();
+console.log('we can use here another team');
+ourTeam();
